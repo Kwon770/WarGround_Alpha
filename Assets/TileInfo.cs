9733bfe4d192c280c33533c37ab91bf08ac9172f
Reofard <6590f1@naver.com>
2018-09-29 00:06:53 +0900

    123
    123

---
+++ Assets/TileInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour {
+
+    [SerializeField] int x;
+    [SerializeField] int y;
+}
