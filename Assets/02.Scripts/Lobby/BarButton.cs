8fc15a6fa102a0cda7b2c28307ff7b7783a7e593
soonchan <soonchan@github.com>
2018-11-19 00:27:17 +0900

    파일 정리, 로비 스크립트 시작

---
+++ Assets/02.Scripts/Lobby/BarButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarButton : MonoBehaviour {
+
+    Bar bar;
+    RectTransform rect;
+
+    private void Start()
+    {
+        rect = gameObject.GetComponent<RectTransform>();
+    }
+
+    void OnPointerEnter()
+    {
+        bar.Selected(transform.position.x, rect.rect.width);
+    }
+
+    void OnPointerExit()
+    {
+
+    }
+
+    void OnClick()
+    {
+
+    }
+}
