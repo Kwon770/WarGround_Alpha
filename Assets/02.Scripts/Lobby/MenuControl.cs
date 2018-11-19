e027f37fcbdb6127ee286179925239f83e371f2f
sketom <sketom@github.com>
2018-11-20 00:43:53 +0900

    로비 디자인 재시작

---
+++ Assets/02.Scripts/Lobby/MenuControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour {
+
+    public GameObject home;
+    [SerializeField] float layoutMoveSpeed;
+    [SerializeField] AnimationCurve curve;
+
+
+    public IEnumerator MenuAnim()
+    {
+        Vector3 startPos, endPos;
+
+        startPos = transform.position;
+        endPos = home.transform.position;
+        float time = 0;
+
+        while (time <= 1)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, curve.Evaluate(time));
+            time += Time.deltaTime * layoutMoveSpeed;
+            yield return null;
+        }
+    }
+}
